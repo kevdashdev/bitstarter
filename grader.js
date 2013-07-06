3608c971c936c24ef90e6fb1a59b3bec0ea1ebf3
kevdashdev <kevdashdev@gmail.com>
2013-07-06 11:09:11 +0800

    Pass all sample test

--- grader.js
+++ grader.js
@@ -49,7 +49,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
   var checks = loadChecks(checksfile).sort(),
       out = {};
   for(var ii in checks) {
-    var present = $(checks[ii].length > 0);
+    var present = $(checks[ii]).length > 0;
     out[checks[ii]] = present;
   }
   return out;
